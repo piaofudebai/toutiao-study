7ab09c7e9caad832c1fb9897c51cc63d745a1c08
piaofudebai <248278307@qq.com>
2022-06-26 09:44:52 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // vue.config.js
+
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '时光头条'
+        return args
+      })
+  }
+
 })
